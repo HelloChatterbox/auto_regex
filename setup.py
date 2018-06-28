79153457b022055ba4df0d9633cf3fc52bf905f9
jarbasal <jarbasai@mailfence.com>
2018-06-28 17:34:51 +0100

    fix pip

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='auto_regex',
-    version='0.2',
-    packages=[''],
+    version='0.2.1',
+    packages=['autoregex'],
     url='https://github.com/JarbasAl/auto_regex',
     license='MIT',
     author='jarbasAI',
